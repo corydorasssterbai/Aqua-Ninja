a9733bb6fcea1e01e3d75c0780376aa97d435661
velove1 <danielleraay@gmail.com>
2025-06-04 07:52:19 +0700

    ReminderWater

--- app/src/main/java/com/kania/aquaninjanew/Home.kt
+++ app/src/main/java/com/kania/aquaninjanew/Home.kt
@@ -5,7 +5,7 @@ import android.os.Bundle
 import android.widget.ImageView
 import androidx.appcompat.app.AppCompatActivity
 
-class Home : AppCompatActivity() {
+class  Home : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
